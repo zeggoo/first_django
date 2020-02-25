2491c7fad9f1dcb2966b3419aa6d7e772006f76f
zeggoo <zeggoo@gmail.com>
2020-02-25 10:19:31 +0900

    Post 모델 추가하기

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Post
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Post)   # 장고걸스 테스트용
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 
 class Question(models.Model):
@@ -22,3 +23,20 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Post(models.Model):
+    # 장고걸스 테스트용
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
