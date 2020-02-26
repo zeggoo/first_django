0dac2bcb22d3cbb6ea17b2bffe25b0ebd96bdd8c
zeggoo <zeggoo@gmail.com>
2020-02-26 16:47:38 +0900

    뷰 개선하고 테스트케이스 적용하기

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 from .models import Question, Choice
 
 
@@ -12,12 +14,15 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         '''return all published questions.'''
-        return Question.objects.order_by('-pub_date')
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
